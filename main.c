96ca1741ae83ab2b2b9ca94459d65253dd898cda
OriBenZaken <orinetanel1@gmail.com>
2018-03-30 09:47:11 +0300

    try

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    printf("Hello, World!\n");
+    char s[] = "ls -l here";
+    char* token = strtok(s, " ");
+    int i = 0;
+    while (token != NULL) {
+        printf("%s\n", token);
+        token = strtok(NULL, " ");
+    }
+    printf("Enter any key to end\n");
+    char end;
+    scanf(" %c", &end);
     return 0;
 }

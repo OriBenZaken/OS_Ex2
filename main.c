3c06250b4cae89efadaad9bfc40f5950e17e7463
OriBenZaken <orinetanel1@gmail.com>
2018-04-01 14:34:54 +0300

    Start working with fork

--- main.c
+++ main.c
@@ -2,8 +2,13 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <wait.h>
 
 #define MAX_COMMAND_ARGS 10
+#define MAX_COMMAND_LENGTH 50
+#define DONT_WAIT 0
+#define WAIT 1
 
 int getNumberOfWords(char* str) {
     int count = 0;
@@ -17,12 +22,13 @@ int getNumberOfWords(char* str) {
 }
 
 char** stringToExecvArgs(char* str) {
-    int numOfWords = getNumberOfWords(str);
+/*    int numOfWords = getNumberOfWords(str);
     char** args = (char**) malloc((numOfWords + 1) * sizeof(char*));
     if (args == NULL) {
         printf("Failed to execute command due to memory problem\n");
         exit(1);
-    }
+    }*/
+    char** args[MAX_COMMAND_ARGS];
     char* token = strtok(str, " ");
     int i = 0;
     while (token != NULL) {
@@ -33,14 +39,49 @@ char** stringToExecvArgs(char* str) {
     return args;
 }
 
+void executeCommand(char** args, int waitFlag) {
+    pid_t pid;
+    // create son process to execute the command
+    pid = fork();
+    if (pid != 0) {
+        printf("%d\n",pid);
+    }
+    // son process
+    if (pid == 0) {
+        printf("bitch\n");
+
+        int i = 0;
+        while(args[i] != NULL) {
+            printf("%s\n", args[i]);
+            i++;
+        }
+        execv(args[0], args);
+    }
+    if (waitFlag && pid != 0) {
+        waitpid(pid, NULL, WCONTINUED);
+    }
+}
+
 int main() {
-    char s[] = "/bin/ls -r -t -l";
-    char** args = stringToExecvArgs(s);
-    printf("%s %s %s\n", args[0], args[1], args[2]);
-/*    printf("Enter any key to end\n");
-    char end;
-    scanf(" %c", &end);*/
-    execv(args[0], args);
-    free(args);
+    char s[MAX_COMMAND_LENGTH];
+    printf("prompt> ");
+    fgets(s, MAX_COMMAND_LENGTH, stdin);
+    // remove new line character
+    s[strlen(s) - 1] = '\0';
+    char** args[MAX_COMMAND_ARGS];
+    int waitFlag = WAIT;
+    char* token = strtok(s, " ");
+    int i = 0;
+    while (token != NULL) {
+        args[i++] = token;
+        token = strtok(NULL, " ");
+    }
+    if (strcmp(args[i - 1],"&") == 0) {
+        waitFlag = DONT_WAIT;
+        args[i - 1] = NULL;
+    } else {
+        args[i] = NULL;
+    }
+    executeCommand(args, waitFlag);
     return 0;
 }
